ef055f449e4578d32184aeb3d5992bec994a1390
Ashley Frieze <ashley@incredible.org.uk>
2021-05-31 07:53:51 +0100

    Initial boilerplate

---
+++ src/main/java/uk/org/webcompere/App.java
@@ -0,0 +1,13 @@
+package uk.org.webcompere;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
