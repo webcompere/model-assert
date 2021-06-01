f01a6dbdc1db346e79566e3af0bb88eb830de910
Ashley Frieze <ashley@incredible.org.uk>
2021-06-01 22:49:31 +0100

    Allow more source JSONs

--- src/main/java/uk/org/webcompere/modelassert/json/JsonProvider.java
+++ src/main/java/uk/org/webcompere/modelassert/json/JsonProvider.java
@@ -5,12 +5,31 @@ import org.opentest4j.AssertionFailedError;
 
 import java.io.IOException;
 
+/**
+ * Provides conversion to JSON of the input
+ * @param <T> type of input
+ */
 @FunctionalInterface
 public interface JsonProvider<T> {
+    /**
+     * Convert the input object into JSON
+     * @param json the json source
+     * @return a loaded {@link JsonNode} from the source
+     * @throws IOException
+     */
     JsonNode from(T json) throws IOException;
 
+    /**
+     * Execute the provider, wrapping exceptions up as assertion failures
+     * @param item the item to unpack - this will be of type <code>T</code>
+     *             but we allow it to be <code>Object</code> as Hamcrest is poorly behaved with type safety
+     * @return the loaded {@link JsonNode} or an assertion failure if the load failed, or the input is null
+     */
     @SuppressWarnings("unchecked") // hamcrest doesn't provide type safety but this object is constrained by generics
     default JsonNode jsonFrom(Object item) {
+        if (item == null) {
+            throw new AssertionFailedError("Input JSON is null");
+        }
         try {
             return from((T)item);
         } catch (ClassCastException | IOException e) {
