624ab3b969488565efa73d9ed1d33c7abf768f2e
Laurent Martelli <martellilaurent@gmail.Com>
2024-11-16 22:15:37 +0100

    Fix javadoc

--- src/main/java/uk/org/webcompere/modelassert/json/dsl/nodespecific/ArrayNodes.java
+++ src/main/java/uk/org/webcompere/modelassert/json/dsl/nodespecific/ArrayNodes.java
@@ -6,7 +6,7 @@ import uk.org.webcompere.modelassert.json.dsl.Satisfies;
 import uk.org.webcompere.modelassert.json.dsl.SubsetDsl;
 
 /**
- * Reduce the DSL down to just Aarrays
+ * Reduce the DSL down to just arrays
  * @param <A> the type of assertion
  */
 public class ArrayNodes<A> extends SubsetDsl<A> implements ArrayNodeDsl<A> {
